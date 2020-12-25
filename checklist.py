39c4a3aea8d4c0438aa89f40466abb541f100d76
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2020-12-25 08:52:07 +0200

    added implementation frame with all elements

--- checklist.py
+++ checklist.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 checklist = Tk()
 checklist.title("SLS Checklist")
-checklist.geometry('600x600')
+checklist.geometry('1080x600')
 
 #Checklist FRAMES
 frame_0 = LabelFrame(checklist) #Study info frame
@@ -11,9 +11,9 @@ frame_0.grid(row=0,column=0,columnspan=2,padx=10,pady=10,ipadx=10,ipady=5)
 frame_1 = LabelFrame(checklist) #Awarded study frame
 frame_1.grid(row=0,column=2,columnspan=2,padx=10,pady=10)
 frame_2 = LabelFrame(checklist) #Localisation setup frame
-frame_2.grid(row=2,column=1,columnspan=3,padx=10,pady=10)
+frame_2.grid(row=0,column=4,columnspan=3,padx=10,pady=10)
 frame_3 = LabelFrame(checklist) #Implementation frame
-frame_3.grid(row=3,column=1,columnspan=2,padx=20,pady=10)
+frame_3.grid(row=2,column=0,columnspan=3,padx=20,pady=10)
 
 
 #Study info frame
@@ -46,7 +46,7 @@ device_type_box.grid(row=5,column=1,pady=(10,0))
 """Awardeded Study frame labels"""
 awarded_study = Label(frame_1,text='Awarded Study')
 internal_kom =Label(frame_1,text='Internal KOM')
-check_wf = Label(frame_1, text="Check WF")
+check_wf = Label(frame_1, text="Check WF plan")
 sls_task = Label(frame_1, text="Assign SLS in WF")
 draft_lp = Label(frame_1, text="Draft loc. plan")
 
@@ -54,7 +54,7 @@ draft_lp = Label(frame_1, text="Draft loc. plan")
 loca_setup = Label(frame_2, text='Localisation setup')
 loca_kom = Label(frame_2, text='Localisation KOM date')
 check_msr = Label(frame_2,text='Check Master SR')
-check_mqrg = Label(frame_2,text='Check Master QRG')
+check_mqrg = Label(frame_2,text='Check Master QRG/SUM')
 check_ldc = Label(frame_2, text='Check master LDC is done')
 check_paper_source = Label(frame_2, text="Check paper sources")
 check_translation_service = Label(frame_2,text="Confirm translation services")
@@ -66,10 +66,13 @@ quote_po_oct = Label(frame_2, text="Add quote and PO in OCT")
 loca_plan = Label(frame_2, text="Create final localisation plan")
 access_ts_tm = Label(frame_2, text="Get access in TS and TM")
 assign_ls_wf = Label(frame_2,text="Assing LS in WF")
+access_plaform = Label(frame_2,text="Get access in vendor platform(if needed)")
 
 """Implementation frame"""
 implementation = Label(frame_3, text="Implementation")
-handover_ls = Label(frame_3, text="Handover LS")
+handover_ls = Label(frame_3, text="Handover info to LS")
+notes = Label(frame_3, text="NOTES")
+notes_text= Text(frame_3, width=40,height=10)
 
 
 
@@ -96,11 +99,13 @@ quote_po_oct.grid(row=5,column=2)
 loca_plan.grid(row=6,column=2)
 access_ts_tm.grid(row=7,column=2)
 assign_ls_wf.grid(row=8,column=2)
-
+access_plaform.grid(row=9,column=2)
 
 """Implementation"""
 implementation.grid(row=0,column=0, columnspan=2, pady=10)
 handover_ls.grid(row=1,column=0)
+notes.grid(row=2,column=0,columnspan=2)
+notes_text.grid(row=3,column=0,columnspan=2,padx=10,pady=5)
 
 #Variables for checkboxes
 """Awarded studies"""
@@ -125,6 +130,9 @@ quote_po_oct_var = IntVar()
 loca_plan_var = IntVar()
 access_ts_tm_var = IntVar()
 assign_ls_wf_var = IntVar()
+access_plaform_var = IntVar()
+"""Implementation variables"""
+handover_ls_var=IntVar()
 
 #CHECKBOXES
 """Awardeded Study frame checkboxes"""
@@ -137,7 +145,7 @@ draft_lp_cb = Checkbutton(frame_1, variable=draft_lp_var)
 loca_kom_cb = Checkbutton(frame_2, variable=loca_kom_var)
 check_msr_cb = Checkbutton(frame_2, variable=check_msr_var)
 check_mqrg_cb = Checkbutton(frame_2, variable=check_mqrg_var)
-check_ldc_cb = Checkbutton(frame_2, variable=c_ldc_var)
+check_ldc_cb = Checkbutton(frame_2, variable=check_ldc_var)
 check_paper_source_cb = Checkbutton(frame_2, variable=check_paper_source_var)
 check_translation_service_cb = Checkbutton(frame_2, variable=check_translation_service_var)
 check_tft_cb = Checkbutton(frame_2, variable=check_tft_var)
@@ -148,6 +156,10 @@ quote_po_oct_cb = Checkbutton(frame_2, variable=quote_po_oct_var)
 loca_plan_cb = Checkbutton(frame_2, variable=loca_plan_var)
 access_ts_tm_cb = Checkbutton(frame_2, variable=access_ts_tm_var)
 assign_ls_wf_cb = Checkbutton(frame_2, variable=assign_ls_wf_var)
+access_plaform_cb=Checkbutton(frame_2, variable=access_plaform_var)
+
+"""Implementation"""
+handover_ls_cb =Checkbutton(frame_3, variable=handover_ls_var)
 
 #CHECKBOXES GRID
 """Awarded study"""
@@ -171,6 +183,9 @@ quote_po_oct_cb.grid(row=5,column=3)
 loca_plan_cb.grid(row=6,column=3)
 access_ts_tm_cb.grid(row=7,column=3)
 assign_ls_wf_cb.grid(row=8,column=3)
+access_plaform_cb.grid(row=9,column=3)
+"""Inplementation"""
+handover_ls_cb.grid(row=1,column=1)
 
 
 checklist.mainloop()
